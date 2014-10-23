33fbfea3dfe4bade82b3d94985662ff6394c93a9
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2014-10-23 13:45:21 +0300

    Added "Packet" page

--- js/script.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    var elem = $('#heder');
-    setInterval(function(){
-        if (elem.css('color') === 'rgb(66, 75, 255)')elem.css('color', 'rgb(253, 55, 55)');
-        else elem.css('color', 'rgb(66, 75, 255)');
-    }, 500);
-
-})();
